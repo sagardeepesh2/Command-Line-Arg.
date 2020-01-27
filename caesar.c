31e64b0278391d083d9c720ef2a4964a1dc696ac
sagardeepesh2 <53931842+sagardeepesh2@users.noreply.github.com>
2020-01-27 21:25:16 +0530

    Add files via upload

---
+++ caesar.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    // check for 2 arguments only
+    if (argc != 2)
+    {
+        printf("%s key", argv[0]);
+        return 1;
+    }
+    
+    // once I check for correct argv put key into an int k
+    int k = atoi(argv[1]);
+
+    // check if the integer is non-negative
+    if (k < 0)
+    {
+        printf("%s key", argv[0]);
+        return 1;
+    }
+    else
+    {
+        // prompt user for a code to encrypt
+        string code = get_string("plaintext: ");
+        printf("ciphertext: ");
+        for (int i = 0, n = strlen(code); i < n; i++)
+        {
+            //check if the letter is uppercase or lowercase then convert
+            if islower(code[i])
+            { 
+                printf("%c", (((code[i] + k) - 97) % 26) + 97);
+            }    
+            else if isupper(code[i])
+            { 
+                printf("%c", (((code[i] + k) - 65) % 26) + 65);
+            }  
+            //if neither then just print whatever it is
+            else
+            {       
+                printf("%c", code[i]);
+            }
+        }
+           
+        printf("\n");
+        return 0;
+    }
+}
